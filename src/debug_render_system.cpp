5a19f271f44724e990f2f05f27a800f7ac1b6e30
a pirate <jesse.hughes.it@gmail.com>
2023-10-14 08:21:57 -0600

    Move some functions into stdlib

--- src/debug_render_system.cpp
+++ src/debug_render_system.cpp
@@ -313,55 +313,6 @@ BufferScopeTreeEntry(debug_ui_render_group *Group, debug_profile_scope *Scope,
 /* } */
 /* #endif */
 
-link_internal void
-PushCycleBar(debug_ui_render_group* Group, cycle_range* Range, cycle_range* Frame, r32 TotalGraphWidth, r32 BarHeight, r32 yOffset, ui_style *Style, v4 Padding = V4(0), cs Name = CSz(""))
-{
-  Assert(Frame->StartCycle <= Range->StartCycle);
-
-  r32 FramePerc = (r32)Range->TotalCycles / (r32)Frame->TotalCycles;
-
-  r32 BarWidth = FramePerc*TotalGraphWidth;
-
-  if (BarWidth > 0.15f)
-  {
-    v2 BarDim = V2(BarWidth, BarHeight);
-
-    u64 StartCycleOffset = Range->StartCycle - Frame->StartCycle;
-    r32 xOffset = GetXOffsetForHorizontalBar(StartCycleOffset, Frame->TotalCycles, TotalGraphWidth);
-
-    v2 Offset = V2(xOffset, yOffset);
-
-    PushUntexturedQuad(Group, Offset, BarDim, zDepth_Text, Style, Padding, QuadRenderParam_AdvanceClip);
-    ui_style NameStyle = DefaultStyle;
-    NameStyle.Font = Global_SmallFont;
-    if (Name.Count) { Text(Group, Name, &NameStyle, TextRenderParam_NoAdvanceLayout, Offset, RectMinDim({}, BarDim)); }
-  }
-
-  return;
-}
-
-link_internal u8
-GetByte(u32 ByteIndex, u64 Source)
-{
-  u32 ShiftWidth = ByteIndex * 8;
-  Assert(ShiftWidth < (sizeof(Source)*8));
-
-  u64 Mask = (u64)(0xff << ShiftWidth);
-  u8 Result = (u8)((Source & Mask) >> ShiftWidth);
-  return Result;
-}
-
-link_internal v3
-ColorFromHash(u64 HashValue)
-{
-  u8 R = GetByte(0, HashValue);
-  u8 G = GetByte(1, HashValue);
-  u8 B = GetByte(2, HashValue);
-
-  v3 Color = V3( R / 255.0f, G / 255.0f, B / 255.0f );
-  return Color;
-}
-
 global_variable r32 Global_CoreBarHeight = 3.f;
 global_variable r32 Global_CoreBarPadding = 3.f;
 
@@ -379,11 +330,11 @@ PushScopeBarsRecursive( debug_ui_render_group *Group,
     cycle_range Range = {Scope->StartingCycle, GetCycleCount(Scope)};
 
     /* umm NameHash = Hash(CS(Scope->Name)) ^ Hash(CS(Scope->Location)); */
-    umm NameHash = Hash(CS(Scope->Name));
 
     random_series ScopeSeries = {.Seed = (u64)Scope};
     r32 Tint = RandomBetween(0.5f, &ScopeSeries, 1.0f);
 
+    umm NameHash = Hash(CS(Scope->Name));
     v3 FunctionColor = ColorFromHash(NameHash) * Tint;
     ui_style FunctionStyle = UiStyleFromLightestColor(FunctionColor);
     r32 yOffsetFunction = Depth * BarHeight;
